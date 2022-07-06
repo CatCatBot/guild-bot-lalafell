a42344e49f4da6879661398b45fb5b1646148b75
sayari <loli@loli.monster>
2022-07-06 17:52:13 +0800

    config

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { createOpenAPI, createWebsocket } from 'qq-guild-bot';
+import { lalafellConfig } from './bot/config/lalafell.config';
 
+const client = createOpenAPI(lalafellConfig);
+const websocket = createWebsocket(lalafellConfig);
 @Injectable()
 export class AppService {
   getHello(): string {
     return 'Hello World!';
+  }
+  sendMsg(msg: string): string {
+    client.meApi
+      .meGuilds()
+      .then((res) => {
+        console.log(res.data);
+        client.channelApi.channels(res.data[0].id).then((res) => {
+          res.data.forEach((channel) => {
+            if (channel.name === '测试聊天') {
+              client.messageApi
+                .postMessage(channel.id, {
+                  content: msg,
+                })
+                .then((res) => {
+                  console.log(res.data);
+                })
+                .catch((err) => {
+                  console.log(err);
+                });
+            }
+          });
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    return msg;
   }
 }
---
+++ src/bot/config/lalafell.config.ts
@@ -0,0 +1,6 @@
+const lalafellConfig = {
+  appID: 'xxx',
+  token: 'xxx',
+  sandbox: true, // optional, default false
+};
+export { lalafellConfig };
