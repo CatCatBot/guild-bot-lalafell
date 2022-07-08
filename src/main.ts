c2acea4e0515894cec5955d28bf6d36909341012
sayari <loli@loli.monster>
2022-07-08 16:29:00 +0800

    role select

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ import {
 } from 'qq-guild-bot';
 import { lalafellConfig, baseConfig } from './bot/config/lalafell.config';
 
+const roleEmoji = [307, 306, 277, 198, 206, 204, 185];
+const roleIds = [];
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
@@ -19,9 +22,19 @@ function initBot() {
   const ws = createWebsocket(lalafellConfig);
   ws.on(
     AvailableIntentsEventsEnum.GUILD_MESSAGES,
-    (data: { msg: IMessage }) => {
+    async (data: { msg: IMessage }) => {
       console.log(data);
+      const channelID = data.msg.channel_id;
+      const guildID = data.msg.guild_id;
       const content = data.msg?.content;
+      if (content?.includes('debug')) {
+        const targetID = content.split(' ')[1];
+        const { data: debugMsg } = await client.messageApi.message(
+          channelID,
+          targetID,
+        );
+        console.debug(debugMsg);
+      }
       if (content?.includes('ping')) {
         client.messageApi.postMessage(data.msg.channel_id, {
           content: 'pong',
@@ -30,21 +43,52 @@ function initBot() {
       if (content?.includes(`<@!${baseConfig.robotId}>`)) {
         if (content?.includes('/hi')) {
           client.messageApi.postMessage(data.msg.channel_id, {
-            content: 'Hello!',
+            content: '您好，莉莉菈为您服务！',
           });
         }
       }
       if (content?.includes(`<@!${baseConfig.robotId}>`)) {
-        if (content?.includes('/role')) {
-          client.messageApi
-            .postMessage(data.msg.channel_id, {
+        try {
+          if (content?.includes('/role')) {
+            // 查询所有身份组
+            const { data: rolesData } = await client.roleApi.roles(guildID);
+            const roles = rolesData.roles;
+            console.info(roles);
+            const selectRoles = roles.filter(
+              (role) =>
+                !role.name.includes('创建者') &&
+                !role.name.includes('管理员') &&
+                !role.name.includes('普通成员'),
+            );
+            let rolesMsg =
+              '长按或右击消息,\n通过添加对应表情,\n选择你的角色吧!\n';
+            let emojiSort = 0;
+            for (let index = 0; index < selectRoles.length; index++) {
+              const role = selectRoles[index];
+              roleIds.push(role.id);
+              rolesMsg += `选择 <emoji:${roleEmoji[emojiSort]}> 领取 ${role.name}\n`;
+              emojiSort++;
+            }
+            if (selectRoles.length === 0) {
+              rolesMsg = '暂无可选身份组,请先添加~';
+            }
+            client.messageApi
+              .postMessage(data.msg.channel_id, {
+                content: rolesMsg,
+              })
+              .then((res) => {
+                console.info(res.data);
+              })
+              .catch(console.error);
+          }
+        } catch (e) {
+          console.error(e);
+          if (e.code === 11264) {
+            client.messageApi.postMessage(data.msg.channel_id, {
               content:
-                '选择你的角色吧!\n 选择 <emoji:307> 领取 猫猫 \n 选择 <emoji:306> 领取 拉拉肥',
-            })
-            .then((res) => {
-              console.info(res.data);
-            })
-            .catch(console.error);
+                '莉莉菈权限不够啦，请在权限设置中，将机器人身份设置为管理员~\n（非子频道管理员）',
+            });
+          }
         }
       }
     },
@@ -66,83 +110,56 @@ function initBot() {
         userID,
       );
       const nick = data1.nick;
-      client.roleApi.roles(guildID).then((res) => {
-        const roles = res.data.roles;
-        client.messageApi
-          .message(channelID, messageID)
-          .then(async (res) => {
-            console.info(res.data);
-            if (res.data.message.content.includes('选择你的角色吧')) {
-              if (emoji === '307') {
-                const { data } = await client.guildApi.guildMember(
+      const { data: roleData } = await client.roleApi.roles(guildID);
+      const roles = roleData.roles;
+      const { data: targetMsg } = await client.messageApi.message(
+        channelID,
+        messageID,
+      );
+      // console.info(targetMsg);
+      if (targetMsg.message.content.includes('选择你的角色吧')) {
+        const { data: guildMember } = await client.guildApi.guildMember(
+          guildID,
+          userID,
+        );
+        let validRole = false;
+        console.info(roleIds);
+        roleEmoji.find((emojiId) => {
+          if (emojiId.toString() === emoji) {
+            validRole = true;
+            return true;
+          }
+        });
+        if (validRole) {
+          for (let index = 0; index < roleIds.length; index++) {
+            const emojiId = roleEmoji[index];
+            const roleId = roleIds[index];
+            const role = roles.find((role) => role.id === roleId);
+            if (emojiId.toString() === emoji) {
+              if (
+                guildMember.roles.includes(
+                  roles.find((role) => role.id === roleId).id.toString(),
+                )
+              ) {
+                client.messageApi.postMessage(channelID, {
+                  content: `${nick} 你已经是 ${role.name} 了<emoji:271>`,
+                });
+              } else {
+                console.info('add role');
+                await client.messageApi.postMessage(channelID, {
+                  content: `${nick} 你选择了 ${role.name}<emoji:179>`,
+                });
+                client.memberApi.memberAddRole(
                   guildID,
+                  roleId,
                   userID,
+                  channelID,
                 );
-                if (
-                  data.roles.includes(
-                    roles.find((role) => role.name === '猫猫').id.toString(),
-                  )
-                ) {
-                  client.messageApi
-                    .postMessage(channelID, {
-                      content: `${nick} 你已经是 猫猫 了<emoji:271>`,
-                    })
-                    .then((res) => {
-                      console.info(res.data);
-                    })
-                    .catch(console.error);
-                } else {
-                  client.messageApi
-                    .postMessage(channelID, {
-                      content: `${nick} 你选择了 猫猫<emoji:179>`,
-                    })
-                    .then((res) => {
-                      client.memberApi.memberAddRole(
-                        guildID,
-                        roles.find((role) => role.name === '猫猫').id,
-                        userID,
-                        channelID,
-                      );
-                    });
-                }
-              }
-              if (emoji === '306') {
-                const { data } = await client.guildApi.guildMember(
-                  guildID,
-                  userID,
-                );
-                if (
-                  data.roles.includes(
-                    roles.find((role) => role.name === '拉拉肥').id.toString(),
-                  )
-                ) {
-                  client.messageApi
-                    .postMessage(channelID, {
-                      content: `${nick} 你已经是 拉拉肥 了<emoji:271>`,
-                    })
-                    .then((res) => {
-                      console.info(res.data);
-                    })
-                    .catch(console.error);
-                } else {
-                  client.messageApi
-                    .postMessage(channelID, {
-                      content: `${nick} 你选择了 拉拉肥<emoji:179>`,
-                    })
-                    .then((res) => {
-                      client.memberApi.memberAddRole(
-                        guildID,
-                        roles.find((role) => role.name === '拉拉肥').id,
-                        userID,
-                        channelID,
-                      );
-                    });
-                }
               }
             }
-          })
-          .catch(console.error);
-      });
+          }
+        }
+      }
     },
   );
 }
