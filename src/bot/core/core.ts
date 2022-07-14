4d67a7104de8f26ca39bf7d84e79cd4b820fad21
sayari <loli@loli.monster>
2022-07-14 15:24:05 +0800

    add help plugin

--- src/bot/core/core.ts
+++ src/bot/core/core.ts
@@ -1,4 +1,5 @@
 import queryFish from '../plugins/ffxiv-fish/ffxiv.fish';
+import helpText from '../plugins/help-text/help.text';
 import nlpReply from '../plugins/nlp-reply/nlp.reply';
 import rolePlay from '../plugins/role-play/role-play';
 
@@ -8,6 +9,7 @@ const loadPlugins = async (
 ) => {
   console.info('loading plugins...');
   let spread = true;
+  spread = await helpText(client, data, spread);
   spread = await queryFish(client, data, spread);
   spread = await rolePlay(client, data, spread);
   await nlpReply(client, data, spread);
---
+++ src/bot/plugins/help-text/help.text.ts
@@ -0,0 +1,28 @@
+import { baseConfig } from 'src/bot/config/lalafell.config';
+import { postImage } from 'src/bot/ext/post';
+
+const helpText = async (
+  client: any,
+  data: { eventType: string; msg: any },
+  spread: boolean,
+) => {
+  if (data.eventType === 'MESSAGE_CREATE' && spread) {
+    console.log(data);
+    const channelID = data.msg.channel_id;
+    const guildID = data.msg.guild_id;
+    const content = data.msg?.content;
+    if (content?.includes(`<@!${baseConfig.robotId}>`)) {
+      if (content?.includes('help') || content?.includes('帮助')) {
+        // postImage(data.msg, 'help.png');
+        client.messageApi.postMessage(channelID, {
+          content:
+            '您好，莉莉菈为您服务！\n📕@bot help||帮助 可以查看帮助\n🐟 道具检索 鱼名 -> 可以查询鱼类\n👴 @bot /role -> 可以获取身份组\n🕐 @bot dc utterance intent -> 可以添加自定义意图\n🎗️ @bot aws intent answer -> 可以添加自定义回答 \n🪐 @bot train -> 可以重新训练NLP',
+          msg_id: data.msg.id,
+        });
+        spread = false; // msg will not be spreaded to other plugins
+      }
+    }
+  }
+  return spread;
+};
+export default helpText;
