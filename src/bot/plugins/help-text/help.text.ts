78ef882bc730144808af9800171db0f6fdd3f264
loveloliii <loli@loli.monster>
2022-07-19 20:38:30 +0800

    add help text

--- src/bot/plugins/help-text/help.text.ts
+++ src/bot/plugins/help-text/help.text.ts
@@ -16,15 +16,16 @@ const helpText = async (
         // postImage(data.msg, 'help.png');
         client.messageApi.postMessage(channelID, {
           content:
-            '您好，莉莉菈为您服务！\n📕@bot help||帮助 可以查看帮助\n'
-            + '🐟 道具检索 鱼名 -> 可以查询鱼类\n'
-            + '👴 @bot /role -> 可以获取身份组\n'
-            + '👻@bot wls ->list words from db \n'
-            + '🕐 @bot dc utterance intent -> 可以添加自定义意图\n'
-            + '🎗️ @bot aws intent answer -> 可以添加自定义回答 \n'
-            + '🪐 @bot train -> 可以重新训练NLP\n'
-            + '📔 @bot pic 搜索名称 -> 可以搜索Pixiv图片\n'
-            + '📗 @bot dataset 分类名称 [图片,图片, ...] -> 可以添加自定义分类数据集\n',
+            '您好，莉莉菈为您服务！\n📕@bot help||帮助 可以查看帮助\n' +
+            '📕@bot 拉拉肥||私信bot 拉拉肥  可以获取随机拉拉肥美照\n' +
+            '🐟 道具检索 鱼名 -> 可以查询鱼类\n' +
+            '👴 @bot /role -> 可以获取身份组\n' +
+            '👻@bot wls ->list words from db \n' +
+            '🕐 @bot dc utterance intent -> 可以添加自定义意图\n' +
+            '🎗️ @bot aws intent answer -> 可以添加自定义回答 \n' +
+            '🪐 @bot train -> 可以重新训练NLP\n' +
+            '📔 @bot pic 搜索名称 -> 可以搜索Pixiv图片\n' +
+            '📗 @bot dataset 分类名称 [图片,图片, ...] -> 可以添加自定义分类数据集\n',
           msg_id: data.msg.id,
         });
         spread = false; // msg will not be spreaded to other plugins
