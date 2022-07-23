b52e390efeac4386894dbf83006a2514f63ee291
sayari <loli@loli.monster>
2022-07-23 16:58:31 +0800

    fix auto shell

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -14,7 +14,7 @@ cp -R src/bot/utils/nsfw/model dist/bot/utils/nsfw/model
 mkdir dist/bot/plugins/ffxiv-fish/data
 cp src/bot/plugins/ffxiv-fish/data/Item.json dist/bot/plugins/ffxiv-fish/data/Item.json
 mkdir dist/bot/plugins/huajianji/data
-cp src/bot/plugins/huajianji/data/*.json dist/bot/plugins/huajianji/data/*.json
+cp src/bot/plugins/huajianji/data/* dist/bot/plugins/huajianji/data/
 #启动服务
 pm2 delete all
 pm2 start npm --name lalafellBot -- start
